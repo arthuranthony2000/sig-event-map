292d20a394cde2d5aed4d21bcaac2dd392b99a3a
Adalberto Maia <btowmaia@gmail.com>
2022-06-06 23:57:40 -0300

    config inicial com testes #23

---
+++ backend/src/main/java/com/aminterprise/mutlu/MutluApplication.java
@@ -0,0 +1,13 @@
+package com.aminterprise.mutlu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutluApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/aminterprise/mutlu/AmdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aminterprise.mutlu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
