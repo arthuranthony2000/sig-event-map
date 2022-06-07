19e49c8489de55dbd78179e5614656a9b9af5da1
Adalberto Maia <btowmaia@gmail.com>
2022-06-07 00:37:58 -0300

    Criei o service user #30

---
+++ backend/src/main/java/com/aminterprise/mutlu/services/UserService.java
@@ -0,0 +1,72 @@
+package com.aminterprise.mutlu.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.aminterprise.mutlu.dto.EventDTO;
+import com.aminterprise.mutlu.dto.UserDTO;
+import com.aminterprise.mutlu.entities.Event;
+import com.aminterprise.mutlu.entities.User;
+import com.aminterprise.mutlu.repositories.EventRepository;
+import com.aminterprise.mutlu.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	@Autowired
+	private EventRepository eventRepository;
+	
+	@Transactional(readOnly = true)
+	public List<UserDTO> findAll(){
+		List<User> list = repository.findAll();
+		return list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+	}
+	
+	@Transactional(readOnly = true)
+	public List<UserDTO> findUsers(){
+		List<User> list = repository.findUsersActiveOrAdm();
+		return list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+	}
+	
+	@Transactional
+	public User loginUser(String email) {
+		User user = repository.findByEmail(email);
+		return user;
+	}
+	
+	@Transactional
+	public UserDTO insert(UserDTO dto) {
+		User user = new User(
+				null,
+				dto.getName(),
+				dto.getEmail(),
+				dto.getCpf(),
+				dto.getPass(),
+				dto.getStatus());
+		for (EventDTO p : dto.getEvents()) {
+			Event event = eventRepository.getOne(p.getId());
+			user.getEvents().add(event);
+		}
+		user = repository.save(user);
+		return new UserDTO(user);
+	}
+	
+	@Transactional
+	public UserDTO updateUser(Long id, UserDTO newUser) {
+		User user = repository.getOne(id);
+		user.setCpf(newUser.getCpf());
+		user.setEmail(newUser.getEmail());
+		user.setName(newUser.getName());
+		user.setPass(newUser.getPass());
+		user.setStatus(newUser.getStatus());
+		user = repository.save(user);
+		return new UserDTO(user);
+	}
+}
