6f3affdf1407c54a8f1e2427dd526706ab26b679
Adalberto Maia <btowmaia@gmail.com>
2022-06-07 00:21:15 -0300

    Criei o controller event #28

---
+++ backend/src/main/java/com/aminterprise/mutlu/controllers/EventController.java
@@ -0,0 +1,65 @@
+package com.aminterprise.mutlu.controllers;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.aminterprise.mutlu.dto.EventDTO;
+import com.aminterprise.mutlu.services.EventService;
+
+@RestController
+@RequestMapping(value = "/events")
+public class EventController {
+	
+	@Autowired
+	private EventService service;
+	
+	@PostMapping
+	public ResponseEntity<EventDTO> insert(@RequestBody EventDTO dto){
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").
+				buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
+	
+	
+	@GetMapping
+	public ResponseEntity<List<EventDTO>> findActive(){
+		List<EventDTO> list = service.findActive();
+		return ResponseEntity.ok().body(list);
+	}
+	 
+	@GetMapping("/all")
+	public ResponseEntity<List<EventDTO>> findAll(){
+		List<EventDTO> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<EventDTO> updateEvent(@PathVariable Long id, @RequestBody EventDTO newEvent){
+		newEvent = service.updateEvent(id, newEvent);
+		return ResponseEntity.ok().body(newEvent);
+	}
+	
+	@PutMapping("/{id}/conclused")
+	public ResponseEntity<EventDTO> setConclused(@PathVariable Long id){
+		EventDTO dto = service.setConclused(id);
+		return ResponseEntity.ok().body(dto);
+	}
+	
+	@PutMapping("/{id}/pending")
+	public ResponseEntity<EventDTO> setPending(@PathVariable Long id){
+		EventDTO dto = service.setPending(id);
+		return ResponseEntity.ok().body(dto);
+	}
+}
