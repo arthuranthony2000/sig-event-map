f4cf5ec4edac885dfcb4253b12511d78f42a6e57
Arthur Anthony <arthuranthony2000@gmail.com>
2022-07-19 12:26:33 -0300

    Fix #69

---
+++ backend/src/test/java/com/aminterprise/mutlu/controllers/UserControllerTest.java
@@ -0,0 +1,67 @@
+
+package com.aminterprise.mutlu.controllers;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerTest {
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Test
+	public void testFindAllUsers() throws Exception {
+		mockMvc
+				.perform(MockMvcRequestBuilders.get("/users/all"))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON));
+	}
+	
+	@Test
+	public void testFindById() throws Exception {
+		mockMvc
+				.perform(MockMvcRequestBuilders.get("/users?id=1"))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON));
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
