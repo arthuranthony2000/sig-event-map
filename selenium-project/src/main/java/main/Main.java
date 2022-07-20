426d6fe4053a320535bd102485fd9c041c5f65b0
Adalberto Maia <btowmaia@gmail.com>
2022-07-20 17:49:04 -0300

    fix #74

---
+++ selenium-project/src/main/java/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import acceptance_tests.Driver;
+
+public class Main {
+ public static WebDriver driver;
+ 
+ public static void main(String args[]) throws InterruptedException {
+	 System.setProperty("webdriver.chrome.driver", "/home/ufrn/Downloads/chromedriver_linux64/chromedriver");
+	 driver = new ChromeDriver();
+	 
+ }
+}
