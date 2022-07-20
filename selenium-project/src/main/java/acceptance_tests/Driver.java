f2328377d111020b24212075136c74fa0c41d137
Adalberto Maia <btowmaia@gmail.com>
2022-07-20 18:18:16 -0300

    fixed #77

--- selenium-project/src/main/java/acceptance_tests/Driver.java
+++ selenium-project/src/main/java/acceptance_tests/Driver.java
@@ -21,4 +21,26 @@ public class Driver {
 		Main.driver.findElement(By.xpath("//*[@id=\"root\"]/nav/span/div/button")).click();
 		Main.driver.findElement(By.xpath("/html/body/div[2]/div[3]/ul/a[1]")).click();
 	}
+	
+	public static void viewMap() throws InterruptedException {
+		Main.driver.get("http://localhost:3000/");
+		Thread.sleep(5000);
+		Main.driver.findElement(By.className("btn_eventos")).click();
+		Main.driver.findElement(By.xpath("/html/body/div[2]/div[3]/div/div/div[2]/div[2]/button")).click();
+
+		Thread.sleep(8000);
+		Main.driver.findElement(By.xpath("//*[@id=\"login_field\"]")).sendKeys("arthur.fmc.2000@gmail.com");
+		Main.driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("adasdh1280");
+		Main.driver.findElement(By.xpath("//*[@id=\"login\"]/div[3]/form/div/input[12]")).click();
+
+		Thread.sleep(5000);
+		Main.driver.findElement(By.xpath("//*[@id=\"root\"]/nav/span/div/button")).click();
+		Main.driver.findElement(By.xpath("/html/body/div[2]/div[3]/ul/a[2]")).click();
+
+		Thread.sleep(5000);
+		Main.driver.findElement(By.xpath("//*[@id=\"react-select-3-input\"]"))
+				.sendKeys("Caicó, Rio Grande do Norte, Brazil");
+		Thread.sleep(3000);
+		Main.driver.findElement(By.xpath("//*[@id=\"react-select-3-input\"]")).sendKeys(Keys.ENTER);
+	}
 }
